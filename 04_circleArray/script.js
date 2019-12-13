1f57dab9514b4e33e37c806315d4d05be91b0f31
djsjollema <j.sjollema@ma-web.nl>
2019-12-13 12:09:18 +0100

    forloop

--- 04_circleArray/script.js
+++ 04_circleArray/script.js
@@ -10,44 +10,56 @@ const height = window.innerHeight;
 canvas.width = width;
 canvas.height = height;
 
-let circleObject = {};
-circleObject.x = 400;
-circleObject.y = 200;
-circleObject.radius = 40;
-circleObject.color = "purple";
-circleObject.speedX = 6;
-circleObject.speedY = 7;
+circles = [];
 
-circleObject.draw = function(){
-  context.beginPath();
-  context.lineWidth = "5";
-  context.fillStyle = circleObject.color;
-  context.arc(circleObject.x,circleObject.y,circleObject.radius,0,2*Math.PI);
-  context.closePath();
-  context.stroke();
-  context.fill();
+for(let i =0; i<10 ;i++){
+  let circleObject = {};
+  circleObject.x = getRandomInt(0,width);
+  circleObject.y = getRandomInt(0,height);
+  circleObject.radius = 40;
+  circleObject.color = "purple";
+  circleObject.speedX = 6;
+  circleObject.speedY = 7;
+
+  circleObject.draw = function(){
+    context.beginPath();
+    context.lineWidth = "5";
+    context.fillStyle = circleObject.color;
+    context.arc(circleObject.x,circleObject.y,circleObject.radius,0,2*Math.PI);
+    context.closePath();
+    context.stroke();
+    context.fill();
+  }
+  circleObject.update = function(){
+    circleObject.x = circleObject.x + circleObject.speedX;
+    circleObject.y = circleObject.y + circleObject.speedY;
+
+    if(circleObject.y > height - circleObject.radius){
+      circleObject.speedY = - circleObject.speedY
+    }
+
+    if(circleObject.y < 0){
+      circleObject.speedY =  -circleObject.speedY
+    }
+
+    if(circleObject.x > width - circleObject.radius){
+      circleObject.speedX = - circleObject.speedX;
+    }
+
+    if(circleObject.x < 0){
+      circleObject.speedX =  -circleObject.speedX
+    }
+  }
+
+  circles.push(circleObject);
+
 }
 
-circleObject.update = function(){
-  circleObject.x = circleObject.x + circleObject.speedX;
-  circleObject.y = circleObject.y + circleObject.speedY;
 
-  if(circleObject.y > height - circleObject.radius){
-    circleObject.speedY = - circleObject.speedY
-  }
 
-  if(circleObject.y < 0){
-    circleObject.speedY =  -circleObject.speedY
-  }
 
-  if(circleObject.x > width - circleObject.radius){
-    circleObject.speedX = - circleObject.speedX;
-  }
 
-  if(circleObject.x < 0){
-    circleObject.speedX =  -circleObject.speedX
-  }
-}
+
 
 function anime(){
   context.clearRect(0,0,width,height);
@@ -55,4 +67,11 @@ function anime(){
   circleObject.draw();
 }
 
-setInterval(anime,10)
+//setInterval(anime,10)
+
+
+function getRandomInt(min,max){
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random()*(max-min+1) + min);
+}
